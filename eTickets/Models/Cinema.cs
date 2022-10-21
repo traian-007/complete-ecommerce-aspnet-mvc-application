18bd4375e3bef862f827affbaae1a93bb0603983
Andrian <traian-007@mail.ru>
2022-10-21 07:47:01 +0300

    createAddCinema

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -8,11 +8,14 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name="Name")]
+        [Display(Name="Cinema Name")]
+        [Required(ErrorMessage = "Cinema name is required")]
         public string Name { get; set; }
         [Display(Name = "Cinema Log")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         // Relationships
