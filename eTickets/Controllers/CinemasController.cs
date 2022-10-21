5237019fa7c09588d1a7592d2af4d10002bfef85
Andrian <traian-007@mail.ru>
2022-10-21 08:05:09 +0300

    addDetailsToCinema

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -34,5 +34,13 @@ namespace eTickets.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinemas/Details
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
     }
 }
