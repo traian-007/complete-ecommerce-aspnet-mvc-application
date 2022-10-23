0fe209bc826d23f0d5c81c6c0f76f1ff732da482
Andrian <traian-007@mail.ru>
2022-10-23 10:13:27 +0300

    settingShoppingCartClass

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCart> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
