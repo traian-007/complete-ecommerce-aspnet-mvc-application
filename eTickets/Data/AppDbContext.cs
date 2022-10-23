8974572c4794d49a5bf39759a0ee42b57a8da4e8
Andrian <traian-007@mail.ru>
2022-10-23 09:48:08 +0300

    addShoppingCartItemModel

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -35,5 +35,6 @@ namespace eTickets.Data
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
