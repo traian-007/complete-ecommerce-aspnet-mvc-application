cd8efff0bb5a51207384f4488a923f58b29b4b03
Andrian <traian-007@mail.ru>
2022-10-23 14:42:33 +0300

    addingAndConfiguringOrdersService

--- eTickets/Data/ViewComponents/ShoppingCartSummary.cs
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -8,7 +8,7 @@ namespace eTickets.Data.ViewComponents
         private readonly ShoppingCart _shoppingCart;
         public ShoppingCartSummary(ShoppingCart shoppingCart)
         {
-            _shoppingCart = shoppingCart;
+            _shoppingCart = shoppingCart; 
         }
 
         public IViewComponentResult  Invoke()
