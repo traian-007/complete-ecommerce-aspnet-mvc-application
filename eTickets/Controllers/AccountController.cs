1e4248d3b9484802325d41a97e952a69abe5a02a
Andrian <traian-007@mail.ru>
2022-11-12 20:13:51 +0200

    Signing out user

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -86,5 +86,12 @@ namespace eTickets.Controllers
 
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
